8cf4348c73eac4e6cdad8d42e730ebe448890eb7
Tracey061 <75406009+Tracey061@users.noreply.github.com>
2021-07-25 16:40:07 +1000

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP Basics</title>
+</head>
+<body>
+<h1>PHP Basics</h1>
+<?php
+echo "Hello";
+?>
+</body>
+</html>
---
+++ time.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dispaly the time</title>
+</head>
+<body>
+    <h1>Dispaly the time</h1>
+    <?php
+    $currentTime = date("g:i:s a");
+
+    echo "Hello the time is $currentTime";
+    ?>
+
+</body>
+</html>
