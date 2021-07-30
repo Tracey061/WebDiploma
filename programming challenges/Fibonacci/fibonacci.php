816109253004b5c863b7120e14c516042b55fd90
Tracey061 <chunxuewangsw@foxmail.com>
2021-07-30 15:52:15 +1000

    change2 fibonacci

---
+++ programming challenges/Fibonacci/fibonacci.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    function fibonacciFun($i,$j,$k)
+    {    
+        $fibonacciArray[0]=$i;
+        $fibonacciArray[1]=$j;
+
+        for($m=2;$m<$k+2;$m++)
+        {
+            $sum=$i+$j;
+            $fibonacciArray[$m]=$sum;
+            $i=$j;
+            $j=$sum;
+        }
+        
+        return $fibonacciArray;
+    }
+
+    $resultArray = fibonacciFun(0,1,20);
+    // print_r($resultArray);
+
+    for($i=0;$i<count($resultArray);$i++)
+    {
+        $output=$resultArray[$i];
+        include "templates/fibonacci.html.php";
+    }
+    ?>
+</body>
+</html>
---
+++ programming challenges/Fibonacci/templates/fibonacci.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>  
+
+    <!-- when section is repalced by P, the cssstyle didn't work. Why -->
+    <section><?= $output ?></section>
+</body>
+</html>
